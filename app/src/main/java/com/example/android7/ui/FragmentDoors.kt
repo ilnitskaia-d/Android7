32203cd7360118dff04b59a6b9d906ad0b1e06bf
Daria Ilnitskaia <ilnitskaia_d@auca.kg>
2023-11-27 17:32:30 +0600

    Finish FragmentDoors

--- app/src/main/java/com/example/android7/ui/FragmentDoors.kt
+++ app/src/main/java/com/example/android7/ui/FragmentDoors.kt
@@ -5,14 +5,44 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.android7.R
+import com.example.android7.databinding.FragmentDoorsBinding
+import com.example.android7.ui.adapter.RVAdapter
+import com.example.android7.ui.model.Item
 
 class FragmentDoors : Fragment() {
+    private lateinit var binding: FragmentDoorsBinding
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.fragment_doors, container, false)
+        binding = FragmentDoorsBinding.inflate(layoutInflater)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        val rvAdapter = RVAdapter(
+            listOf(
+                Item("header", "Корридор", ""),
+                Item("item", "Дверь 1", ""),
+                Item("item", "Дверь 2", ""),
+                Item("item", "Дверь 3", ""),
+                Item("header", "Кухня", ""),
+                Item("item", "Дверь 4", ""),
+                Item("item", "Дверь 5", ""),
+            )
+        )
+
+        binding.rv.apply {
+            adapter = rvAdapter
+            layoutManager = LinearLayoutManager(
+                this@FragmentDoors.context,
+                LinearLayoutManager.VERTICAL,
+                false
+            )
+        }
     }
 }
